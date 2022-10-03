a8a7dedef367533b67acc0520f50a777ab49d8e3
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-10-03 12:34:47 +0300

    Book Catalog MicroService completed

--- src/Services/Catalog/Program.cs
+++ src/Services/Catalog/Program.cs
@@ -1,3 +1,6 @@
+using Catalog.Application;
+using Catalog.DataAccess.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IBookRepository, FakeBookRepository>();
+builder.Services.AddScoped<IBookService, BookService>();
 
 var app = builder.Build();
 
